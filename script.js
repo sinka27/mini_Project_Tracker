5a532d96a6faf71c0c608969e0194ab357272e9b
Parul Raj <70625598+sinka27@users.noreply.github.com>
2022-01-06 13:03:03 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var saveChanges = $('#saveChanges')
+//Added event listener to Add project button
+// $( "#button" ).on( "click", function() {
+//     console.log( "hi" );
+//   });
+
+window.setInterval(function (){
+    $('#currentDay').html(moment().format('ddd MM/DD/y H:mm:ss'))
+}, 1000);
+
+$( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+
+console.log(saveChanges)
